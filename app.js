56afa2d150ef2124cab234bedfd11ff8b2291462
shreyansh <guptashreyansh22@gmail.com>
2020-07-02 14:16:03 +0530

    updated heroku port

--- app.js
+++ app.js
@@ -66,9 +66,13 @@ app.get("/",function(req,res){
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
 
-app.listen(3000,function(){
+app.listen(port,function(){
   console.log("server has started");
 })
